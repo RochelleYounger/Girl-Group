8eac76e8d918d78de7a54c814baeea80a60f9ac8
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-04 17:58:23 -0400

    useForm updates

--- motivate/src/components/useForm.js
+++ motivate/src/components/useForm.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useForm = () => {
+const useForm = (validate) => {
   const [values, setValues] = useState({
     username: "",
     email: "",
@@ -19,7 +19,13 @@ const useForm = () => {
     });
   };
 
-  return { handleChange, values };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setErrors(validate(values));
+  };
+
+  return { handleChange, values, handleSubmit, errors };
 };
 
 export default useForm;
