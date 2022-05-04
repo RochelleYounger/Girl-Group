0a05963ad64d864bd9ebc1749281908a8955b983
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-04 17:56:42 -0400

    validate input

---
+++ motivate/src/validate.js
@@ -0,0 +1,27 @@
+export default function validateInfo(values) {
+  let errors = {};
+
+  if (!values.username.trim()) {
+    errors.username = "Username required";
+  }
+
+  if (!values.email) {
+    errors.email = "Email required";
+  } else if (!/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/i.test(values.email)) {
+    errors.email = "Email address is incorrect";
+  }
+
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 8) {
+    errors.password = "Password must be at 8 characters";
+  }
+
+  if (!values.password2) {
+    errors.password2 = "Password is required";
+  } else if (values.password2 !== values.password) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return errors;
+}
