4d27f70fbf38e195c4a5a6c8cfb4a373ad116719
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-04 09:58:01 -0400

    sign up

--- motivate/src/components/Signup.js
+++ motivate/src/components/Signup.js
@@ -1,15 +1,35 @@
 import React from "react";
 
-const Signup = () => {
-  return (
-    <div className="container">
-      <div className="app.wrapper">
-        <div>
-          <h2 className="title">Signup</h2>
+//export as a class not default, because it will be imported inside the index.jsx
+export class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="main-container" ref={this.props.contarinerRef}>
+        <div className="header">Sign Up</div>
+        <div className="content">
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="username">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+          </div>
         </div>
+        <div className="footer"></div>
+        <button type="button" className="btn">
+          Sign Up
+        </button>
       </div>
-    </div>
-  );
-};
-
-export default Signup;
+    );
+  }
+}
