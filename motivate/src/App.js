34a11654bda1ccbe7ecee83c10ca68e060145f49
Habon Assoweh <habonassoweh@Habons-Air>
2022-05-04 17:02:32 -0400

    add Journey

--- motivate/src/App.js
+++ motivate/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import AddJourney from "./components/AddJourney";
 import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <AddJourney />
     </div>
   );
 }
---
+++ motivate/src/components/AddJourney.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddJourney = () => {
+  return (
+    <div>
+      <h1>Add Journey</h1>
+    </div>
+  );
+};
+
+export default AddJourney;
